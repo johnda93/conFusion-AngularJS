02df6447e50f55de6bbb8d4e2af283a0f6f90fd0
johnda93 <jodcastrori@unal.edu.co>
2016-06-07 18:39:39 -0500

    JSHint, concatenation and minification Tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,53 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     changed = require('gulp-changed'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
-    del = require('del');
+    del = require('del'),
+    run_sequence = require('run-sequence');
+
+gulp.task('jshint', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('clean', function () {
+    return del(['dist']);
+});
+
+gulp.task('usemin', ['jshint'], function () {
+    return gulp.src('./app/menu.html')
+        .pipe(usemin({
+            css:[cleancss(), rev()],
+            js: [uglify(), rev()]
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('imagemin', function () {
+    return gulp.src('app/images/**/*')
+        .pipe(cache(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('copyfonts', function () {
+    gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+    
+    gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('build', function (callback) {
+    run_sequence('clean',
+        ['usemin', 'imagemin', 'copyfonts'],
+        callback
+    );
+});

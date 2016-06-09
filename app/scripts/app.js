9b509781395ed0606b1eee14b27363994c48da9d
johnda93 <jodcastrori@unal.edu.co>
2016-06-09 00:47:21 -0500

    Implementing form validation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -75,8 +75,40 @@ app.controller('ContactController', ['$scope', function ($scope) {
         agree: false,
         email: ""
     };
+
+    $scope.channels = [
+        {
+            value:"tel",
+            label:"Tel."
+        },
+        {
+            value:"Email",
+            label:"Email"
+        }
+    ];
+
+    $scope.invalidChannelSelection = false;
 }]);
 
 app.controller('FeedbackController', ['$scope', function ($scope) {
+    $scope.sendFeedback = function () {
+        console.log($scope.feedback);
+        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+            $scope.invalidChannelSelection = true;
+            console.log('incorrect');
+        } else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback = {
+                mychannel: "",
+                firstName: "",
+                lastName: "",
+                agree: false,
+                email: ""
+            };
+            $scope.feedback.mychannel="";
 
+            $scope.feedbackForm.$setPristine();
+            console.log($scope.feedback);
+        }
+    };
 }]);
